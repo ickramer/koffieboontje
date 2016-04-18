bbad11545672dc6bc0f46e7d40259d9578550b31
Iris Kramer <ick.kramer@gmail.com>
2016-04-18 21:07:45 +0200

    update menu text for gamma

--- build/javascripts/all.js
+++ build/javascripts/all.js
@@ -12703,6 +12703,20 @@ return jQuery;
 
 
 
+$(function(){
+
+  $(".tab").on("click", function(event){
+    // Change active tab
+    event.preventDefault();
+    $(".tab").removeClass("active");
+    $(this).addClass("active");
+    // Hide all tab-content (use class="hidden")
+    $(".tab-content").addClass("hidden");
+    // Show target tab-content (use class="hidden")
+    $($(this).data("target")).removeClass("hidden");
+  });
+
+});
 
 
 
